ac8c9523872d805f7128814a97f7ae9bcfa52d6a
Robert Powell <repowell1@crimson.ua.edu>
2024-10-24 17:59:51 -0500

    Add project files.

---
+++ WebDevProject3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebDevProject3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
